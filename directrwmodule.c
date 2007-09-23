66eb2c8610dc0585d95bb5182170689662adc790
Joshua Root <josh+code@root.id.au>
2007-09-23 17:33:09 +0000

    retrying endlessly may not be the best idea
    git-archimport-id: jmr@cse.unsw.edu.au--devel/iomkc--devel--0.1--patch-6

--- directrwmodule.c
+++ directrwmodule.c
@@ -12,30 +12,36 @@ static char directrw_read__doc__[] =
 static PyObject *
 directrw_read(PyObject *self /* Not used */, PyObject *args)
 {
-      int fd = 0;
-      size_t sz = 0;
-      char *buf;
-      ssize_t done = 0;
-
+        int fd = 0;
+        size_t sz = 0;
+        char *buf;
+        ssize_t done = 0;
+        
 	if (!PyArg_ParseTuple(args, "il", &fd, &sz))
 		return NULL;
-
+        
+        //printf("directrw_read: args ok - fd=%d, sz=%ul\n", fd, sz);
       /* valloc aligns to page size; Linux read/write with O_DIRECT
          only really needs 512-byte alignment, but meh */
-      buf = valloc(sz);
-      if (!buf)
-            return PyErr_NoMemory();
-      
-      do {
-            ssize_t n = read(fd, buf+done, sz-done);
-            if (n == -1) {
-                  free(buf);
-                  return PyErr_SetFromErrno(PyExc_OSError);
-            }
-            done += n;
-      } while (done < sz);
-      
-	return Py_BuildValue("s", buf);
+        buf = valloc(sz);
+        if (!buf)
+                return PyErr_NoMemory();
+        
+        //printf("directrw_read: memory allocated ok\n");
+        //do {
+        ssize_t n = read(fd, buf+done, sz-done);
+                //printf("directrw_read: read ok\n");
+        if (n == -1) {
+                free(buf);
+                return PyErr_SetFromErrno(PyExc_OSError);
+        }
+                //printf("directrw_read: read ok\n");
+                //done += n;
+        //} while (done < sz);
+        
+        //printf("directrw_read: all read ok\n");
+        
+        return Py_BuildValue("s", buf);
 }
 
 static char directrw_write__doc__[] =
@@ -45,47 +51,52 @@ static char directrw_write__doc__[] =
 static PyObject *
 directrw_write(PyObject *self /* Not used */, PyObject *args)
 {
-      int fd = 0;
-      size_t sz = 0;
-      char *abuf = NULL; /* correctly aligned buffer */
-      char *buf = NULL; /* original buffer */
-      ssize_t done = 0;
+        int fd = 0;
+        size_t sz = 0;
+        char *abuf = NULL; /* correctly aligned buffer */
+        char *buf = NULL; /* original buffer */
+        ssize_t done = 0;
       
 	if (!PyArg_ParseTuple(args, "isl", &fd, &buf, &sz))
 		return NULL;
 	
+        //printf("directrw_write: args ok - fd=%d, buf=%p, sz=%ul\n", fd, buf, sz);
 	if (((unsigned long)buf & 511) != 0) {
 	     /* misaligned, so fix up */
-	     abuf = valloc(sz);
-	     if (!abuf)
-                  return PyErr_NoMemory();
-            memcpy(abuf, buf, sz);
+                printf("directrw_write: buf misaligned\n");
+                abuf = valloc(sz);
+                if (!abuf)
+                        return PyErr_NoMemory();
+                memcpy(abuf, buf, sz);
 	} else {
-	     /* was OK, so use the existing buffer */
-	     abuf = buf;
+                /* was OK, so use the existing buffer */
+                printf("directrw_write: buf ok\n");
+                abuf = buf;
 	}
 	
-	do {
-            ssize_t n = write(fd, buf+done, sz-done);
-            if (n == -1) {
-                  if (abuf)
+	//do {
+        ssize_t n = write(fd, buf+done, sz-done);
+        //printf("directrw_write: write ok\n");
+        if (n == -1) {
+                if (abuf)
                         free(abuf);
-                  return PyErr_SetFromErrno(PyExc_OSError);
-            }
-            done += n;
-      } while (done < sz);
+                return PyErr_SetFromErrno(PyExc_OSError);
+        }
+            //done += n;
+      //} while (done < sz);
+        //printf("directrw_write: all written ok\n");
 	
 	if (abuf)
-	     free(abuf);
-      Py_INCREF(Py_None);
-      return Py_None;
+                free(abuf);
+        Py_INCREF(Py_None);
+        return Py_None;
 }
 
 /* List of methods defined in the module */
 
 static struct PyMethodDef directrw_methods[] = {
 	{"read",	(PyCFunction)directrw_read,	METH_VARARGS,	directrw_read__doc__},
- {"write",	(PyCFunction)directrw_write,	METH_VARARGS,	directrw_write__doc__},
+        {"write",	(PyCFunction)directrw_write,	METH_VARARGS,	directrw_write__doc__},
  
 	{NULL,	 (PyCFunction)NULL, 0, NULL}		/* sentinel */
 };
@@ -118,4 +129,3 @@ initdirectrw()
 	if (PyErr_Occurred())
 		Py_FatalError("can't initialize module directrw");
 }
-
